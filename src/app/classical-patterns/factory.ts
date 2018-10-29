e5adc35f754285a9d46ac6c6521259cea44991b9
Isaac Luis <luismori@outlook.com>
2018-10-29 15:36:19 -0500

    Create factory.ts

---
+++ src/app/classical-patterns/factory.ts
@@ -0,0 +1,24 @@
+ class User{
+     constructor(private lastName:string, private firstName:string){
+     }
+     hello(){
+         console.log("Hi I am", this.firstName, this.lastName);
+     }
+ }
+ 
+ export class POTOFactory{
+
+     /**
+      * Builds an User from json response
+      * @param  {any}  jsonUser
+      * @return {User}         
+      */
+     static buildUser(jsonUser: any): User {
+
+         return new User(
+             jsonUser.firstName,
+             jsonUser.lastName
+         );
+     }
+
+ }
